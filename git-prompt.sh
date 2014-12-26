3a89102254264df65e58a0b43c0c38ba54173eed
Octowl <alsaff1987@gmail.com>
2014-12-26 10:20:11 +0300

    fixed spacing in git prompt

--- git-prompt.sh
+++ git-prompt.sh
@@ -19,9 +19,9 @@ function git_branch {
 
 	if [[ $git_status =~ $on_branch ]]; then
 		local branch=${BASH_REMATCH[1]}
-		echo "($branch)"
+		echo " ($branch)"
 	elif [[ $git_status =~ $on_commit ]]; then
 		local commit=${BASH_REMATCH[1]}
-		echo "($commit)"
+		echo " ($commit)"
 	fi
 }
